879fd5234f9dc96c557d0884abaf8e667cef5d69
Brian Ward <brianward@duck.com>
2026-04-01 18:10:28 -0500

    Remove comments and fetch function from worker
    Removed original comments and export function from the worker file.

--- cloudflare.openclaw.worker.js
+++ cloudflare.openclaw.worker.js
@@ -1,17 +1,4 @@
 /*
-Originally Created to pass headers through Cloudflare Tunnel to OpenClaw as an alternative to pairing requests
+Moved to -> OpenClaw-Cloudflare-Tunnel-Access/worker-static-files/cloudflare.openclaw.worker.js
 */
 // ---- Last Update 2026.04.01 ----
-
-export default {
-  async fetch(request) {
-    const email = request.headers.get("cf-access-authenticated-user-email");
-    const forwarded = new Request(request);
-
-    if (email) {
-      forwarded.headers.set("x-forwarded-user", email);
-    }
-
-    return fetch(forwarded);
-  },
-};
